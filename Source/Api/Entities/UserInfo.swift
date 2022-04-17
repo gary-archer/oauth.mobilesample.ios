d85c808ad6d340a0611020fc976b1a4923bc8606
gary <gary@MACSTATION.local>
2022-04-17 19:23:40 +0100

    Updates

--- Source/Api/Entities/UserInfo.swift
+++ Source/Api/Entities/UserInfo.swift
@@ -2,6 +2,11 @@
  * User info received from the API
  */
 struct UserInfo: Decodable {
+
+    // These values originate from OAuth user info
     let givenName: String
     let familyName: String
+
+    // This value originates from the API's own data
+    let regions: [String]
 }
