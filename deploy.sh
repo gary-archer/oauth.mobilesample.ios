916bd0a1c9e45e115a3a61c57217b7cfb60bbb9e
gary <gary@MACSTATION.local>
2022-06-02 14:17:15 +0100

    Updates

---
+++ deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+####################################################################################################
+# A script to build and deploy the release build of the IPA file to the connected emulator or device
+####################################################################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+#
+# Do a clean
+#
+rm -rf build
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered cleaning the iOS build system'
+  exit
+fi
+
+#
+# Do a release build to produce an archive file
+#
+xcodebuild -workspace basicmobileapp.xcworkspace -scheme basicmobileapp -configuration Release archive -archivePath build/basicmobileapp.xcarchive
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered building the iOS app'
+  exit
+fi
+
+#
+# Export the archive to an IPA file, which requires my code signing certificate
+# https://ajpagente.github.io/mobile/xcb-with-xcconfig/
+#
+xcodebuild -exportArchive -archivePath build/basicmobileapp.xcarchive -exportPath build -exportOptionsPlist export.plist
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered building the iOS app to an archive file'
+  exit
+fi
+
+#
+# Uninstall on an emulator or device if required
+#
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered uninstalling the iOS app'
+  exit
+fi
+
+#
+# Deploy to the connected device
+#
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered deploying the iOS app'
+  exit
+fi
+
+#
+# Then run the app
+#
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered running the iOS app'
+  exit
+fi
